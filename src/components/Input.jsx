d1fc8469116ea1a8c818ea1ad1639cf0ad329fd7
tamara-munoz <tami.skt@gmail.com>
2021-08-25 13:46:35 -0400

    test: creating mock prop types to test the input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Input = (props) => {
+const Input = ({ secretWord }) => {
     return (
         <div data-test="component-input" >
             <input type="text" placeholder="Enter a word" />
         </div>
     )
+};
+
+Input.propTypes = {
+    secretWord: PropTypes.string.isRequired
 }
 
 export default Input;
--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -3,17 +3,14 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../../test/testUtils';
 import Input from './Input';
 
-const defaultProps = { success: false };
-
 /**
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
  * @param {object} props - Component props specific to this setup.
  * @returns {ShallowWrapper}
  */
-const setup = (props = {}) => {
-    const setupProps = { ...defaultProps, ...props };
-    return shallow(<Input {...setupProps} />)
+const setup = (secretWord = 'party') => {
+    return shallow(<Input secretWord={secretWord} />)
 }
 
 test('render without error ', () => {
@@ -21,3 +18,19 @@ test('render without error ', () => {
     const inputComponent = findByTestAttr(wrapper, 'component-input');
     expect(inputComponent.length).toBe(1);
 });
+
+test('does not throw warning with expeted props', () => {
+    checkProps(Input, { secretWord: 'party' });
+});
+
+// describe('', () => {
+//     let wrapper
+//     beforeEach(() => {
+//         wrapper = setup({ guessedWords: [] });
+//     });
+
+//     test('renders without error', () => {
+//         const component = findByTestAttr(wrapper, 'component-input');
+//         expect(component.length).toBe(1);
+//     });
+// });
