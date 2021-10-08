956018f8c3cb1e3f0a71688afa71e817705264a4
tamara-munoz <tami.skt@gmail.com>
2021-10-08 15:48:24 -0300

    feat: Add successReducer test and code

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,10 +1,15 @@
+import { actionTypes } from '../actions';
 /**
  * @function sucessReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - action to be reduced.
  * @returns {boolean} - new success state.
  */
-
-export default (state, action) => {
-    return null;
+export default (state = false, action) => {
+    switch (action.type) {
+        case (actionTypes.CORRECT_GUESS):
+            return true;
+        default:
+            return state;
+    }
 }
