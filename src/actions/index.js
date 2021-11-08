25acbf69c9c2530e928c5b3ba533d966af50ef9f
tamara-munoz <tami.skt@gmail.com>
2021-11-08 13:39:27 -0300

    test: set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,32 @@ import axios from 'axios';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-}
+    GUESS_WORD: 'GUESS_WORD',
+};
+
+// /**
+//  * @function correctGuess
+//  * @returns {object} - Action object with type 'CORRECT_GUESS'
+//  */
+// export function correctGuess() {
+//     return { type: actionTypes.CORRECT_GUESS };
+// }
 
 /**
- * @function correctGuess
- * @returns {object} - Action object with type 'CORRECT_GUESS'
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *    and (conditionally) GUESS_WORD action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux thunk function.
  */
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = () => {
+    return function(dispatch, getState) {
+
+    };
+};
 
 export const getSecretWord = () => {
     // TODO write actual action in Redux / Context section
     return axios.get('http://localhost:3030')
         .then(response => response.data);
-}
+};
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,13 +1,16 @@
 import moxios from 'moxios';
-import { getSecretWord, correctGuess, actionTypes } from './';
+import {
+    getSecretWord,
+    // correctGuess,
+    // actionTypes,
+} from './';
 
-describe('correctGuess', () => {
-    test('returns an action with type ´CORRECT_GUESS´', () => {
-        const action = correctGuess();
-        expect(action).toStrictEqual({ type: actionTypes.CORRECT_GUESS });
-    });
-    
-});
+// describe('correctGuess', () => {
+//     test('returns an action with type ´CORRECT_GUESS´', () => {
+//         const action = correctGuess();
+//         expect(action).toStrictEqual({ type: actionTypes.CORRECT_GUESS });
+//     });
+// });
 
 describe('getSecretWord', () => {
     beforeEach(() => {
